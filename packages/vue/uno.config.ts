a503d2b46460b58a18e1a7999fbd1d68b074e978
giacomohuang <giacomohuang@qq.com>
2026-07-02 15:50:54 +0800

    refactor: 删除不再使用的 simplebar.css 文件，更新 uno.config.ts 以优化导入格式

--- packages/vue/uno.config.ts
+++ packages/vue/uno.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig, transformerDirectives, transformerVariantGroup } from 'unocss'
+import { defineConfig, transformerDirectives, transformerVariantGroup } from "unocss";
 
-import { presetUnoUI } from './src/uno'
+import { presetUnoUI } from "./src/uno";
 
 export default defineConfig({
   presets: [presetUnoUI()],
   transformers: [transformerDirectives(), transformerVariantGroup()],
   content: {
     pipeline: {
-      include: [
-        /\.(vue|svelte|[jt]sx|vine.ts|mdx?|astro|elm|php|phtml|marko|html)($|\?)/,
-        'src/**/*.{js,ts}'
-      ],
-      exclude: ['uno.config.ts']
-    }
-  }
-})
+      include: [/\.(vue|svelte|[jt]sx|vine.ts|mdx?|astro|elm|css|php|phtml|marko|html)($|\?)/, "src/**/*.{js,ts}"],
+      exclude: ["uno.config.ts"],
+    },
+  },
+});
